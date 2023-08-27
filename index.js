3e2f523dcb9876a8a9eaaf56881c832fa0ad3542
gbcarlos <carlos.giese@web.de>
2023-08-27 21:49:34 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const PORT = 8000
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json('Welcome to Polar Backend')
+})
+
+app.listen(PORT, () => console.log("Server running on PORT 8000"))
