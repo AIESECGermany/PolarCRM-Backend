c24fb6dcad04624ca4d92a82006ac350fe8274f7
gbcarlos <carlos.giese@web.de>
2023-09-03 22:24:32 +0900

    Added endpoint for submit form

--- routes/ApplicantRoutes.js
+++ routes/ApplicantRoutes.js
@@ -1,41 +1,9 @@
-const express = require('express')
-const Applicant = require('../models/Applicant')
+import { listApplicants, newApplicant } from '../controllers/applicants.controllers.js'
+import express from 'express'
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Applicant Routes are working')
-})
+router.get('/', listApplicants)
+router.post('/new', newApplicant)
 
-router.post('/new', (req, res) => {
-    
-    try {
-        const { 
-            firstName,
-            familyName,
-            lc,
-            email,
-            telephone,
-            occupation,
-            german,
-            channel,
-            motivation
-        } = req.body
-        const applicant = new Applicant({
-            firstName,
-            familyName,
-            lc,
-            email,
-            telephone,
-            occupation,
-            german,
-            channel,
-            motivation
-        })
-        applicant.save()
-        res.status(201).send({ applicant, message: "Application was recorded!" })
-    } catch(err) {
-        res.status(400).send({ error: err})
-    }
-})
-
-module.exports = router
+export default router
